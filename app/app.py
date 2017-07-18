fccdb83a732ba3597f8b26b6e8255b6c610bea77
bkabiro <briankigondu@gmail.com>
2017-07-18 20:19:00 +0300

    Move Directory of Python Code
    Restructed the file structure so that I could work on projects better

--- app/app.py
+++ app/app.py
@@ -1,22 +1,23 @@
-from flask import Flask,render_template
-app = Flask(__name__,static_url_path='/static',static_folder="static")
+from flask import Flask,render_template,request
+app = Flask(__name__,static_url_path='/static',static_folder="../static",template_folder='../templates')
 
 class Bucketlist():
-	def __init__(self,name,index=0,bucket={}):
+	def __init__(self,name,id=0,bucket={}):
 		self.name = name
-		self.index = index
+		self.id = id
 		self.bucket = bucket 
 	def print_name(self):
 		return self.name
 	def __del__(self):
 		return True
 	def add_item(self,item):
-		if self.index not in self.bucket:
-			self.bucket[self.index] = item
-			self.index = self.index + 1 
+		if self.id not in self.bucket:
+			self.bucket[self.id] = item
+			self.id = self.id + 1 
 	def remove_item(self,index):
 		if index in self.bucket:
 			del self.bucket[index]
+
 
 @app.route('/')
 def index():
@@ -33,6 +34,13 @@ def login():
 @app.route('/view')
 def view():
 	return render_template('view.html')
-	
+
+@app.route('/validate', methods = ['POST'])
+def validate():
+	if request.method == 'POST':
+		data = request.form
+		print(data)
+		return render_template("view.html")
+
 if __name__ == "__main__":
 	app.run()
