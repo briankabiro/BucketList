f66baf485a15fe0767c6222cef5c60e7905d465d
bkabiro <briankigondu@gmail.com>
2017-07-19 17:48:53 +0300

    Add Ajax Function
    Wrote some jquery to send data to the server. Wrote the event handlers to add items to the page

--- static/view.js
+++ static/view.js
@@ -1,9 +1,34 @@
 //create task
 function createItem(text){
-    var markup = '<li><div class="checkbox"><label><input type="checkbox" value="" />'+ text +'</label></div></li>';
-    $('#items-list').append(markup);
+    
+    var items = "";
+    $.ajax({
+      type:'POST',
+      url:"http://localhost:5000/view",
+      data:{
+        method:"add",
+        data:text
+      },
+      success:function(response){
+        console.log("data was sent and received",response);
+        items = response;
+        for (var key in response){
+          console.log(response[key])
+          var markup = '<li><div class="list-item"><span>'+ response[key] +'</span><div class="close">x</div></div></li>';
+          $('#items-list').append(markup);
+        }
+      },
+      error:function(err){
+        console.log("error",err)
+      }
+    })
+    
     $('#add-item-input').val('');
 }
+
+$(document).on('submit', '#title', function() {
+    $(this).removeAttr('disabled');
+});
 
 $("#add-item-form").on('submit',function(e){
 	e.preventDefault()
@@ -14,3 +39,21 @@ $("#add-item-form").on('submit',function(e){
     return
   }
 })
+
+/* 
+  when add button pressed
+  add input title and create button to DOM
+  when create is pressed 
+  send data to server and create bucket list
+  return bucketlist with input and button for adding
+  render bucketlist on frontend
+
+  send add item request to server
+  add item to bucket list
+  return bucket list
+
+  send delete item request to server
+  find a way to get id of the item from the DOM
+  delete item from bucketlist
+  return bucketlist
+*/
