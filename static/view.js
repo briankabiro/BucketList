8a826f32813d4c05695bd72bc750b98dcbed0b26
bkabiro <briankigondu@gmail.com>
2017-07-20 17:17:47 +0300

    Add New Styles
    I changed the colors of the app and the theme. I added the logout button, yet to implement its functionality

--- static/view.js
+++ static/view.js
@@ -1,7 +1,41 @@
-//create task
+  //create task
+let create = false;
+let present = false;
+
+function createBucket(text,title){
+  if (present){
+    $.ajax({
+      type:'POST',
+      url:"http://localhost:5000/view",
+      data:{
+        method:"create",
+        itemBody:text,
+        title:title
+      },
+      success:function(response){
+        console.log("data was sent and received",response);
+        create = true
+        response.forEach(function(itemObject){
+          var markup = '<div class="list-item"><span>'+ itemObject.body +'</span><div class="close-div"><span class="close">x</span></div></div>';
+          $('#items-list').append(markup);        
+        })
+      },
+      error:function(err){
+        console.log("error",err)
+      }
+    })
+  $('#add-item-input').val('');
+  }else{
+    let markup = "<div id='bucket-list'><input type='text' id='title-input' name='title' placeholder='Name of the bucket' required><form id='add-item-form'><input id= 'add-item-input' type='text' name='add-item-input' required><button>Add an Item</button> </form><div id='items-list'></div><button id='delete-button'>DELETE</button></div>"
+    $('bucket-lists').append(markup)
+    present = true
+  }
+}
+
+
 function createItem(text){
-    
-    var items = "";
+    let items = "";
+    //ajax request to send data to server
     $.ajax({
       type:'POST',
       url:"http://localhost:5000/view",
@@ -11,33 +45,91 @@ function createItem(text){
       },
       success:function(response){
         console.log("data was sent and received",response);
-        items = response;
-        for (var key in response){
-          console.log(response[key])
-          var markup = '<li><div class="list-item"><span>'+ response[key] +'</span><div class="close">x</div></div></li>';
+        $('#items-list').html("")
+        response.forEach(function(itemObject){
+          var markup = '<div class="list-item"><span>'+ itemObject.body +'</span><div class="close-div"><span class="close">x</span></div></div>';
           $('#items-list').append(markup);
-        }
+        })
       },
       error:function(err){
         console.log("error",err)
       }
     })
-    
     $('#add-item-input').val('');
 }
+
+
+$('#create-button').click(function(e){
+  //change display-type to 
+  e.preventDefault();
+  if ($('#bucket-list').style.visibility == 'hidden'){
+    $('#bucket-list').style.visibility = 'visible'
+  }else{
+    $('#bucket-list').style.visibility = 'hidden'
+  }
+})
+
+
+
+
+$("#add-item-form").on('submit',function(e){
+  e.preventDefault()
+  var text = $('#add-item-input').val()
+  var title = $('#title-input').val()
+  console.log("these are text and title",text,title)
+  if(text && title){
+    if (create) {
+      createItem(text)
+    }else{
+      createBucket(text,title)
+    }
+  }else{
+    return
+  }
+})
+
+
+$('#delete-button').on('click',function(e){
+  e.preventDefault()
+  if (create){
+    alert("Are you sure you want to delete the bucket?")
+      $.ajax({
+        type:'POST',
+        url:"http://localhost:5000/view",
+        data:{
+          method:"delete"
+        },
+        success:function(response){
+          console.log("data was sent and received",response);
+            $('.bucket-lists').html("")
+        },
+        error:function(err){
+          console.log("error",err)
+        }
+      })
+      present = false
+      create = false
+  }else{
+    return
+  }   
+})
 
 $(document).on('submit', '#title', function() {
     $(this).removeAttr('disabled');
 });
 
-$("#add-item-form").on('submit',function(e){
-	e.preventDefault()
-  var text = $('#add-item-input').val()
-  if(text){
-    createItem(text)
-  }else{
-    return
-  }
+$(document).on('click', '#title', function() {
+    $(this).removeAttr('disabled');
+});
+
+$(".close-div").on('click', function(){
+  //get parent element and remove it from view
+  //send post request to the server with the Id of the element and then remove element from bucket dict
+  //return the new data
+  let parent = (".close").parent()
+  let index = $('div').index(parent)
+  console.log("this is the index",index)
+  console.log("this is the parent",parent)
 })
 
 /* 
