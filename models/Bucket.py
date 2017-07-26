9a83b41c555c6ce99da0ab915ff337126d1afd3a
bkabiro <briankigondu@gmail.com>
2017-07-27 00:07:42 +0300

    Update Classes
    I updated the bucket and user class to fit the functionality that the app has currently

--- models/Bucket.py
+++ models/Bucket.py
@@ -1,24 +1,30 @@
 class Bucket(object):
-    def __init__(self, name, bucket=[]):
+    def __init__(self, name, Id, items=[], count=0):
         self.name = name
-        self.bucket = bucket
+        self.items = items
+        self.Id = Id
+        self.count = count
 
     def return_name(self):
         # print the name of the created user
         print(self.name)
 
+    def update_name(self, new_title):
+        print("this is the old title",self.name)
+        self.name = new_title
+        print("this is the new_title", self.name)
+
     def add_item(self, item):
-        self.bucket.append({"Id": len(self.bucket), "body": item})
+        self.items.append({"Id": self.count, "body": item})
+        self.count += 1
 
     def update_item(self, Id, item):
-        for i in self.bucket:
+        for i in self.items:
             if i['Id'] == Id:
-                i['body'] == item
+                i['body'] = item
 
     def remove_item(self, Id):
-        for i in self.bucket:
-            if i['Id'] == Id:
-                del i
-
-
-
+        for i in self.items:
+            if str(i['Id']) == Id:
+                print('this is i',i)
+                self.items.remove(i)
