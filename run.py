1725d5526497aee5c212626ff31cc46a9d39aca5
bkabiro <briankigondu@gmail.com>
2017-08-03 10:20:30 +0300

    Add Environ Import

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
+from os import environ
 import sys
 sys.path.append(os.getcwd())
 from app import app
 
-app.run(host='0.0.0.0',port=environ.get("PORT", 5000))
+app.run(debug=False,host='0.0.0.0',port=environ.get("PORT", 5000))
