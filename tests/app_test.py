a9a0167d2e894afecac5b6875173236b7860c250
bkabiro <briankigondu@gmail.com>
2017-08-03 12:55:30 +0300

    Fix Tests
    I fixed the tests and added the relevant docstrings

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,66 +1,82 @@
 import unittest
 from app import app
 
+
 class AppTest(unittest.TestCase):
-	def setUp(self):
-		self.client = app.test_client()
-		app.config['WTF_CSRF_ENABLED'] = False
-	# helper methods
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['WTF_CSRF_ENABLED'] = False
 
-	def signup(self,username,password):
-		return self.client.post('/signup',
-			data=dict(username=username, password=password),
-			follow_redirects=True)
-		
-	def login(self,username,password):
-		return self.client.post('/login',data=dict(username=username,password=password),follow_redirects=True)
+    # helper methods
+    def signup(self, username, password):
+        return self.client.post(
+            '/signup',
+            data=dict(username=username, password=password),
+            follow_redirects=True)
 
-	def logout(self):
-		return self.client.post('/logout',follow_redirects=True)
+    def login(self, username, password):
+        return self.client.post(
+            '/login',
+            data=dict(username=username, password=password),
+            follow_redirects=True)
 
-	#Tests
-	def Test_homepage(self):
-		response = self.client.get('/')
-		self.assertEqual(response.status,'200 OK')
+    def logout(self):
+        return self.client.post('/logout', follow_redirects=True)
 
-	def Test_signup_page(self):
-		response = self.client.get('/signup')
-		self.assertEqual(response.status,'200 OK')
+    #Tests
+    def Test_homepage(self):
+        # test if homepage is returned
+        response = self.client.get('/')
+        self.assertEqual(response.status, '200 OK')
 
+    def Test_signup_page(self):
+        # test to check if signup page is live
+        response = self.client.get('/signup')
+        self.assertEqual(response.status, '200 OK')
 
-	def Test_login_page(self):
-		response = self.client.get('/login')
-		self.assertEqual(response.status, '200 OK')
+    def Test_login_page(self):
+        # test to check login page is live
+        response = self.client.get('/login')
+        self.assertEqual(response.status, '200 OK')
 
-	def Test_signup(self):
-		response = self.signup("lee","12345")
-		self.assertIn(b'Home', response.data)
+    def Test_signup(self):
+        # test to check that signup works
+        response = self.signup("lee", "12345")
+        self.assertIn(b'Home', response.data)
 
-	def Test_user_exists(self):
-		response = self.signup('lee','12345')
-		self.assertIn(b'Username already taken', response.data)
+    def Test_user_exists(self):
+        # test to check that username is unique
+        response = self.signup('lee', '12345')
+        self.assertIn(b'Username already taken', response.data)
 
-	def Test_login(self):
-		self.signup('becky','laland')
-		self.logout()
-		response = self.login('becky','laland')
-		self.assertIn(b'Home',response.data)
+    def Test_login(self):
+        # test to check that one can login
+        self.signup('becky', 'laland')
+        self.logout()
+        response = self.login('becky', 'laland')
+        self.assertIn(b'Home', response.data)
 
-	def Test_Invalid_login(self):
-		self.signup('John','13131')
-		self.logout()
-		response = self.login('John','112233')
-		print('this is the response',response.data)
-		self.assertIn(b'Invalid credentials',response.data)
-	
-	def Test_bucket_is_created_and_deleted(self):
-		self.signup('Jane','14141')
-		response = self.client.post('/view/create_bucket',data=dict(bucket="Hiking"),follow_redirects=True)
-		self.assertIn(b'Hiking',response.data)
-		response = self.client.post('/bucket/delete_bucket/0',follow_redirects=True)
-		self.assertNotIn(b'Hiking',response.data)
+    def Test_Invalid_login(self):
+        # test to check that one cannot login with invalid credentials
+        self.signup('John', '13131')
+        self.logout()
+        response = self.login('John', '112233')
+        self.assertIn(b'Invalid credentials', response.data)
 
-	def Test_logout(self):
-		self.signup('john','12345')
-		response = self.logout()
-		self.assertIn(b'Bucket List',response.data)
+    def Test_bucket_is_created_and_deleted(self):
+        # test to check that a bucket is created and deleted when one is logged in
+        self.signup('Jane', '14141')
+        response = self.client.post(
+            '/view/create_bucket',
+            data=dict(bucket="Hiking"),
+            follow_redirects=True)
+        self.assertIn(b'Hiking', response.data)
+        response = self.client.post(
+            '/bucket/delete_bucket/0', follow_redirects=True)
+        self.assertNotIn(b'Hiking', response.data)
+
+    def Test_logout(self):
+        # test to check that logout works
+        self.signup('john', '12345')
+        response = self.logout()
+        self.assertIn(b'Bucket List', response.data)
--- tests/bucket_tests.py
+++ tests/bucket_tests.py
@@ -1,47 +1,54 @@
 import unittest
 from app.models.bucket import Bucket
 
+
 class TddinBucketlist(unittest.TestCase):
     def setUp(self):
-        # create an object
-        self.bucketlist = Bucket("bucketlist",1)
+        # function that runs before each test
+        self.bucket = Bucket('One', 1)
 
     def Test_bucketlist_created(self):
         # test if object is created
-        self.assertEqual(self.bucketlist.name, "bucketlist")
+        self.assertEqual(self.bucket.return_name(), "One")
+
+    def Test_bucket_name_updated(self):
+        self.bucket.update_name('Two')
+        self.assertEqual(self.bucket.name, 'Two')
 
     def Test_unique_id_created(self):
         # test if a unique id is created for each item
-        self.bucketlist.add_item("one")
-        self.assertNotEqual(self.bucketlist.count, 0)
-
+        self.bucket.add_item("one")
+        self.assertNotEqual(self.bucket.count, 0)
 
     def Test_item_added_to_bucket(self):
         # test if an item is added to the object
-        self.bucketlist.add_item("the winner by a knockout is")
-        self.assertEqual(self.bucketlist.items[0]["body"],
+        self.bucket.add_item("the winner by a knockout is")
+        self.assertEqual(self.bucket.items[0]["body"],
                          "the winner by a knockout is")
-    
+        self.bucket.remove_item("0")
+
     def Test_item_updated(self):
-        self.bucketlist1 = Bucket("bucket", 2)
-        self.bucketlist1.add_item('two')
-        self.bucketlist1.update_item(0,'one')
-    
+        # test if the body of an item in bucket list is updated
+        self.bucket.add_item('one')
+        self.bucket.update_item("0", "two")
+        print('tis is self.bucket', self.bucket.items)
+        self.assertEqual(self.bucket.items[0]["body"], 'two')
+        self.bucket.remove_item("0")
+
     def Test_item_removed_from_bucket(self):
         # test if an item is removed from the bucket
-        self.bucketlist.add_item("One")
-        self.bucketlist.remove_item(0)
-        self.assertNotIn(0, self.bucketlist.items)
+        print('this is self.bucket.items', self.bucket.items)
+        self.bucket.add_item('sleep')
+        self.bucket.remove_item("0")
+        self.assertEqual(0, len(self.bucket.items))
 
-    '''
     def Test_multiple_items_added(self):
         # test if multiple items are added to object
-        self.bucketlist = Bucket('bucket',3)
-        self.bucketlist.add_item("One")
-        self.bucketlist.add_item("Two")
-        print("this is self.bucketlist",self.bucketlist.items)
-        self.assertEqual(len(self.bucketlist.items), 4)
-'''
+        self.bucket.add_item("One")
+        self.bucket.add_item("Two")
+        print("this is self.bucket", self.bucket.items)
+        self.assertEqual(len(self.bucket.items), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
