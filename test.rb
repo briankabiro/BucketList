3b2ff5f1ec8df2db146d5b6f17daade54f189149
brian kabiro <briankigondu@gmail.com>
2018-01-09 11:24:37 +0300

    [chore] Test codeclimate

--- test.rb
+++ test.rb
@@ -1,44 +1,89 @@
-module CsvHeaderService
-  extend self
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
 
-  def csv_header_2(phases)
-    header = [
-      "S/N", "UUID", "Greenhouse Candidate ID", "Name", "E-mail", "Gender",
-      "Location", "Program", "Start Date", "Language Stack", "Cycle",
-      "Wk 1 LFA", "Wk 2 LFA", "Wk 1 Status", "Decision 1 Reason",
-      "Decision 1 Comments", "Wk 2 Status", "Decision 2 Reason",
-      "Decision 2 Comments", "Overall Avg", "Values Avg", "Output Avg",
-      "Feedback Avg"
-    ]
+  # Code is not reloaded between requests.
+  config.cache_classes = true
 
-    # append holistic evaluation criteria
-    HolisticEvaluation.max_evaluations.times do
-      header.concat [
-        "Quality", "Quantity", "Initiative", "Communication", "Integration",
-        "EPIC", "Learning Ability"
-      ]
-    end
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
 
-    phases.each do |phase|
-      header.concat phase.assessments.map(&:name)
-    end
+  # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
-    header
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+  config.serve_static_assets = true
+  config.assets.compress = true
+  config.assets.digest = true
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
+  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # config.action_controller.asset_host = 'http://assets.example.com'
+
+  # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
+  # Mount Action Cable outside main process or domain
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and
+  # use secure cookies.
+  config.force_ssl = true
+
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.
+  config.log_level = :debug
+
+  # Prepend all log lines with the following tags.
+  config.log_tags = [:request_id]
+
+  # Use a different cache store in production.
+  # config.cache_store = :mem_cache_store
+
+  # Use a real queuing backend for Active Job (and separate queues per environment)
+  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_name_prefix = "vof-tracker_#{Rails.env}"
+  config.action_mailer.perform_caching = false
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation cannot be found).
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners.
+  config.active_support.deprecation = :notify
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
+
+  # Use a different logger for distributed setups.
+  # require 'syslog/logger'
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  def csv_header_1(phases)
-    header = %w[Biodata Biodata Biodata Biodata Biodata Biodata Biodata
-                Biodata Biodata Biodata Biodata Biodata Decisions Decisions
-                Decisions Decisions Decisions Decisions Decisions
-                Critical-Numbers Critical-Numbers Critical-Numbers
-                Critical-Numbers]
-
-    HolisticEvaluation.max_evaluations.times do |i|
-      7.times { header << "Holistic Evaluation #{i + 1}" }
-    end
-
-    phases.each { |phase| phase.assessments.each { header << phase.name } }
-
-    header
-  end
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
 end
